76058b017fda2c9411ebdac0e33c00af10ca082a
Daniel Jankowski <daniel.jankowski@rub.de>
2026-09-13 13:36:40 +0200

    chore(deps): dont use deprecated isDarwin

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
           buildInputs = [
             # Add additional build inputs here
             pkgs.libiconv
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
             # Additional darwin specific inputs can be set here
           ];
         };
